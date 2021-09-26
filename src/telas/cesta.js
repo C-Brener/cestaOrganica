7cf2f26cf40ccc9925fef0f813b9f1a300b5c8d9
brenermr <brenercaique0806@gmail.com>
2021-09-26 20:14:23 -0300

    CestaOrganica

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width;
 
@@ -8,6 +9,16 @@ export default function Cesta(){
     return<>
         <Image source={topo} style={estilos.topo}/>
         <Text style={estilos.titulo}>Detalhes da Cesta</Text>   
+        
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <View style={estilos.fazenda}>
+                <Image source={logo} style={estilos.imageName}/>
+                <Text style={estilos.nomeFazenda}>Brener Farm</Text>
+            </View>
+            <Text style={estilos.descricao}> Uma cesta com produtos selecionados cuidadosamente da fazenda para sua cozinha</Text>
+            <Text style={estilos.preco}>R$40.00</Text>
+        </View>
     </>
 }
 
@@ -27,6 +38,41 @@ const estilos = StyleSheet.create({
         fontWeight: "bold",
         padding: 16
 
+    },
+    cesta:{
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nome:{
+        color:'#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold"
+    },
+    fazenda:{
+        flexDirection:"row",
+        paddingVertical:12
+    },
+    imageName:{
+        width: 32,
+        height: 32
+    },
+    nomeFazenda:{
+        fontSize: 16,
+        lineHeight:26,
+        marginLeft:12
+    },
+    descricao:{
+        color: "#A3a3a3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco:{
+        color: "#2a9f85",
+        fontWeight: "bold",
+        fontSize:26,
+        lineHeight:42,
+        marginTop:8
     }
 })
 
@@ -34,3 +80,5 @@ const estilos = StyleSheet.create({
 // Para importar uma imagem precisamos transforma-la em um componentte importando-a
 // Não é possivel retornar dois elementos dentro de um return
 // Fragmentos é um estilo de subcomponentes que consegue agrupar outros componentes dentro de um return 
+//O padrão do flexDirection no react natoive é collum, e podemos trocar para row, funciona parecido com o display inline-block
+//Por padrão todos os elementos do react-native já são display flex
