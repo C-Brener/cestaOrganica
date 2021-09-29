e8b493f0e7293c522db7ac43d092e23b538f00c4
brenermr <brenercaique0806@gmail.com>
2021-09-29 19:28:48 -0300

    orgsCesta

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,4 +1,9 @@
 import logo from '../../assets/logo.png'
+import tomate from '../../assets/frutas/Tomate.png'
+import brocolis from '../../assets/frutas/Brócolis.png'
+import batata from '../../assets/frutas/Batata.png'
+import abobora from '../../assets/frutas/Abóbora.png'
+import pepino from '../../assets/frutas/Pepino.png'
 
 
 const cesta = {
@@ -11,6 +16,31 @@ const cesta = {
         nomeFazenda: "Jenny Jack Farm",
         descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda para sua cozinha",
         preco: "R$40,00"
+    },
+    itens:{
+        titulo:"Itens da cesta",
+        lista: [{
+            nome: "Tomate",
+            imagem: tomate,
+        },
+        {
+            nome: "Brócolis",
+            image: brocolis
+        },
+        {
+            nome: "Batata",
+            imagem: batata,
+        },
+        {
+            nome: "Abóbora",
+            imagem: abobora,
+        },
+        {
+            nome: "Pepino",
+            imagem: pepino,
+        }
+
+    ]
     }
 } 
 
